6571996fb19c2262a77b3fd48316b888cb5114a5
isijiga <isijiga@gmail.com>
2025-11-01 11:09:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/proyecto_dam_202510/Dash/DashboardActivity.java
@@ -0,0 +1,62 @@
+package com.example.proyecto_dam_202510.Dash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.BottomNavigationViewKt;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.proyecto_dam_202510.MainActivity;
+import com.example.proyecto_dam_202510.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardActivity extends AppCompatActivity {
+private FirebaseAuth mAuth;
+private FirebaseUser user;
+
+
+
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_dashboard);
+
+    BottomNavigationView bottomNav = findViewById(R.id.toolbar);
+
+    NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+    NavController navController = navHostFragment.getNavController();
+    NavigationUI.setupWithNavController(bottomNav, navController);
+
+
+
+
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut();
+
+                Intent inten = new Intent(getApplicationContext(), MainActivity.class);
+                inten.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(inten);
+                finish();
+
+
+            }});
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/proyecto_dam_202510/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.proyecto_dam_202510;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyecto_dam_202510.Dash.DashboardActivity;
+import com.example.proyecto_dam_202510.login.LoginFragment;
+import com.example.proyecto_dam_202510.login.RegistroFragment;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+        private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        setContentView(R.layout.login);
+
+
+        checkEstado();
+
+
+
+    }
+
+    private void checkEstado() {
+        if(mAuth.getCurrentUser() != null){
+            Toast.makeText(this,"User "+mAuth.getCurrentUser().getEmail() +"conectado",Toast.LENGTH_LONG).show();
+            navegarDash();
+        }
+        else{
+
+            if(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView) == null){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, new LoginFragment())
+                        .commit();
+
+            };
+
+        }
+
+    }
+
+    private void navegarDash() {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
+    public void registroUsuario() {
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentContainerView, new RegistroFragment())
+                    .addToBackStack(null)
+                    .commit();
+
+
+    }
+}
+
+
+
+
